f936ed04ebb2cbc70f67a9b67a0fcf851a42b11a
karthikc <karthikc@ggktech.com>
2017-12-15 21:28:17 +0530

    i18n

--- aurelia-typescript-plugin-master/src/index.ts
+++ aurelia-typescript-plugin-master/src/index.ts
@@ -1,6 +1,35 @@
-import {FrameworkConfiguration} from 'aurelia-framework';
+import {FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
+import {I18N, TCustomAttribute} from 'aurelia-i18n';
+import * as Backend from 'i18next-xhr-backend';
+
 export function configure(aurelia: FrameworkConfiguration) {
-    aurelia.globalResources([
+    aurelia
+        .globalResources([
 
     ]);
+
+    aurelia
+    .plugin('aurelia-i18n', (instance) => {
+        let aliases = ['t', 'i18n'];
+        // add aliases for 't' attribute
+        TCustomAttribute.configureAliases(aliases);
+        
+        // register backend plugin
+        instance.i18next.use(Backend);
+  
+        // adapt options to your needs (see http://i18next.com/docs/options/)
+        // make sure to return the promise of the setup method, in order to guarantee proper loading
+        return instance.setup({
+          backend: {                                  // <-- configure backend settings
+            loadPath: './src/locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
+            
+          },
+          attributes: aliases,
+          lng : 'de',
+          fallbackLng : 'en',
+          debug : false,
+          ns: ['translation','nav', 'common'],
+          defaultNS: 'translation'
+        });
+      });
 }
